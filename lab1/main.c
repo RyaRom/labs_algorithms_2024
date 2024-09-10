66a35a5cd18382f4ee72f207f7d05cca83e599e5
Roman Ryabikin <riabikin.roman@gmail.com>
2024-09-10 15:46:16 +0300

    refactor: better code style for lab requirements

--- lab1/main.c
+++ lab1/main.c
@@ -1,28 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void matrix_columns_sum(int matrix[][3], int *sums, const int rows, const int cols);
+void read_matrix(int **matrix, int cols, int rows);
+
+void matrix_columns_sum(int **matrix, int *sums, int rows, int cols);
+
+void print_array(const int *array, int size);
 
 int main(void) {
-    const int rows = 4;
-    const int cols = 3;
-    int matrix[4][3] = {
-        {1, 2, 3},
-        {1, 2, 3},
-        {1, 2, 3},
-        {1, 2, 3},
-    };
+    int rows, cols;
 
+    printf("Enter number of rows: \n");
+    scanf("%d", &rows);
+    printf("Enter number of columns: \n");
+    scanf("%d", &cols);
+
+    int **matrix = malloc(rows * sizeof(int *));
     int *row_sum = malloc(sizeof(int) * cols);
+    if (matrix == NULL || row_sum == NULL) {
+        perror("Failed to allocate memory");
+        return 1;
+    }
+
+    read_matrix(matrix, cols, rows);
+
     matrix_columns_sum(matrix, row_sum, rows, cols);
 
-    for (int i = 0; i < cols; i++) {
-        printf("%d ", row_sum[i]);
-    }
+    print_array(row_sum, cols);
+
     free(row_sum);
+    free(matrix);
 }
 
-void matrix_columns_sum(int matrix[][3], int *sums, const int rows, const int cols) {
+void print_array(const int *array, const int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", array[i]);
+    }
+}
+
+void read_matrix(int **matrix, const int cols, const int rows) {
+    printf("Enter the matrix elements:\n");
+    for (int i = 0; i < rows; i++) {
+        matrix[i] = malloc(cols * sizeof(int));
+        for (int j = 0; j < cols; j++) {
+            printf("Element [%d][%d]: \n", i, j);
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+}
+
+void matrix_columns_sum(int **matrix, int *sums, const int rows, const int cols) {
     for (int j = 0; j < cols; j++) {
         sums[j] = 0;
         for (int i = 0; i < rows; i++) {
