6e8891205473d191d9a49ea0e83f6cd3c158ee39
Roman Ryabikin <riabikin.roman@gmail.com>
2024-09-11 19:36:16 +0300

    feat: add Queue collection

---
+++ src/collections/Queue.c
@@ -0,0 +1,65 @@
+//
+// Created by locadm on 9/11/2024.
+//
+
+#include "Queue.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+Queue *newQueue() {
+    Queue *queue = malloc(sizeof(Queue));
+    queue->top = NULL;
+    queue->bottom = NULL;
+    queue->size = 0;
+    return queue;
+}
+
+void queue_enqueue(Queue *queue, void *value) {
+    QueueNode *node = malloc(sizeof(QueueNode));
+    node->value = value;
+    node->next = NULL;
+    if (queue->bottom == NULL) {
+        queue->bottom = node;
+        queue->top = node;
+    } else {
+        queue->bottom->next = node;
+        queue->bottom = node;
+    }
+    queue->size++;
+}
+
+void *queue_dequeue(Queue *queue) {
+    if (queue_is_empty(queue)) return NULL;
+
+    QueueNode *top = queue->top;
+    void *value = top->value;
+    QueueNode *newTop = top->next;
+
+    if (newTop == NULL) {
+        queue->bottom = NULL;
+    }
+    queue->top = newTop;
+
+
+    free(top);
+    queue->size--;
+    return value;
+}
+
+void *queue_peek(Queue *queue, void *value) {
+    if (queue_is_empty(queue)) return NULL;
+    return queue->top->value;
+}
+
+void queue_free(Queue *queue) {
+    while (!queue_is_empty(queue)) {
+        queue_dequeue(queue);
+    }
+    free(queue);
+}
+
+int queue_is_empty(const Queue *queue) {
+    if (queue == NULL || queue->size == 0) return 1;
+    return 0;
+}
---
+++ src/collections/Queue.h
@@ -0,0 +1,32 @@
+//
+// Created by locadm on 9/11/2024.
+//
+
+#ifndef QUEUE_H
+#define QUEUE_H
+
+typedef struct QueueNode {
+    void *value;
+    struct QueueNode *next;
+} QueueNode;
+
+typedef struct Queue {
+    QueueNode *top;
+    QueueNode *bottom;
+    int size;
+} Queue;
+
+Queue *newQueue();
+
+void queue_enqueue(Queue *queue, void *value);
+
+void *queue_dequeue(Queue *queue);
+
+void *queue_peek(Queue *queue, void *value);
+
+void queue_free(Queue *queue);
+
+int queue_is_empty(const Queue *queue);
+
+
+#endif //QUEUE_H
