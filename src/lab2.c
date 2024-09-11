84a0bbe77041ba0f022f1aaab4ac0007c1090b30
Roman Ryabikin <riabikin.roman@gmail.com>
2024-09-11 17:39:26 +0300

    feat: add lab 2 main logic

--- src/lab2.c
+++ src/lab2.c
@@ -12,6 +12,8 @@ void lab2() {
     CircularLinkedList *list = newCircularLinkedList();
     List *collector = newList();
     read_int_sequence_stdin(&list->abstract_list, list);
+    printf("%s", "sequence: ");
+    circular_list_display(list);
 
     printf("Enter variable K: \n");
     const int k = 0;
@@ -25,7 +27,7 @@ void collect_sum(List *collector, const CircularLinkedList *list, const int lim)
     Node *current = circular_list_get_first(list);
     if (current == NULL) return;
     int sum = 0;
-    while (sum <= lim) {
+    while (sum < lim) {
         const int data = current->data;
         sum += data;
         list_add_node(collector, data);
